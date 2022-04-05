56a4945ce5f65efbe1ed47c1568c65c1c9ffe4a7
Hamzh Freijat <hamzhg2016@gmail.com>
2022-04-06 02:03:52 +0300

    Under maintain

--- src/main/java/com/example/codefellowship/web/UserController.java
+++ src/main/java/com/example/codefellowship/web/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
@@ -28,11 +29,13 @@ public class UserController {
     @Autowired
     PasswordEncoder encoder;
 
+    @Transactional
     @GetMapping("/")
     String getIndex(Model model){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            ApplicationUser currentUser = applicationUserRepo.findByUsername(userDetails.getUsername());
             model.addAttribute("username" , userDetails.getUsername());
         }
         return "index";
@@ -83,7 +86,7 @@ public class UserController {
         model.addAttribute("username" , userDetails.getUsername());
         model.addAttribute("bio" , applicationUserRepo.findByUsername(userDetails.getUsername()).getBio());
         model.addAttribute("postsList" , applicationUserRepo.findByUsername(userDetails.getUsername()).getPosts());
-        return "profile";
+        return "/myprofile";
     }
 
     @PostMapping("/post")
@@ -91,8 +94,37 @@ public class UserController {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         posts.setUser(applicationUserRepo.findByUsername(userDetails.getUsername()));
         postRepo.save(posts);
-        return new RedirectView("/profile");
+        return new RedirectView("/myprofile");
     }
+
+    @Transactional
+    @GetMapping("/follow/{id}")
+    RedirectView showFollowSuccessScreen(@PathVariable("id") long id, Model model) {
+
+        // user to follow
+        ApplicationUser usertofollow = applicationUserRepo.findById(id).orElseThrow();
+
+        // get current logged in user username
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // use the user name or ID to find the user by username or ID
+        ApplicationUser currentUser = applicationUserRepo.findByUsername(userDetails.getUsername());
+        // once you have that object
+
+        // add the curetn logged in user to the following of usertofollow
+        currentUser.getFollowers().add(usertofollow);
+
+        // add usertofollow to current logged in user followers
+        usertofollow.getFollowing().add(currentUser);
+
+        // update both the userToFollow and the current logged in User to the DB by calling save
+
+        applicationUserRepo.save(usertofollow);
+        applicationUserRepo.save(currentUser);
+
+        return new RedirectView("/");
+    }
+
 
     @GetMapping("/error")
     public String handleError() {
